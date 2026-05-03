f31c774f40fdb3265139a3d61a488ff6f2c0a5cf
Sebanovak <seba.rugby2@gmail.com>
2026-05-03 17:46:05 +0200

    3_commit

--- main.cpp
+++ main.cpp
@@ -1,7 +1,42 @@
 #include <iostream>
+#include <string>
+
+using namespace std;
+
+struct Player {
+    string PlayerClass;
+
+    int maxHP;
+    int hp;
+
+    int maxMana;
+    int mana;
+
+    int gold;
+
+    int level;
+    int xp;
+
+    int attack;
+};
 
 int main() {
-    std::cout << "Ahoj, tohle je muj projekt na GitHubu!" << std::endl;
-    count << "Hello World";
+    player p;
+
+    p.PlayerClass = "Crusader";
+
+      p.maxHP = 10;
+    p.hp = 10;
+
+    p.maxMana = 5;
+    p.mana = 5;
+
+    p.gold = 10;
+
+    p.level = 1;
+    p.xp = 0;
+
+    p.attack = 3;
+
     return 0;
 }
