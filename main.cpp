9006f3e94c3ef2501087cc785a445e395f1f138e
Sebanovak <seba.rugby2@gmail.com>
2026-05-09 17:39:53 +0200

    4_commit

--- main.cpp
+++ main.cpp
@@ -21,22 +21,44 @@ struct Player {
 };
 
 int main() {
-    player p;
+    Player p;
 
+cout << "Vyber si classu:\n";
+    cout << "1 - Crusader\n";
+    cout << "2 - Mage\n";
+
+    int volba;
+    cin >> volba;
+
+else if(volba == 1) {
     p.PlayerClass = "Crusader";
 
-      p.maxHP = 10;
-    p.hp = 10;
+    p.maxHP = 12;
+    p.hp = 12;
 
-    p.maxMana = 5;
-    p.mana = 5;
+    p.maxMana = 3;
+    p.mana = 3;
+
+    p.attack = 4;
 
     p.gold = 10;
-
     p.level = 1;
     p.xp = 0;
+}
+if (volba == 2) {
+    p.PlayerClass = "Mage";
 
-    p.attack = 3;
+    p.maxHP = 6;
+    p.hp = 6;
 
+    p.maxMana = 10;
+    p.mana = 10;
+
+    p.attack = 2;
+
+    p.gold = 10;
+    p.level = 1;
+    p.xp = 0;
+}
     return 0;
 }
