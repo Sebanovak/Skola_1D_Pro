2b7608bf2e6b5083cf05fd7f34d42a0e9dae2745
Sebanovak <seba.rugby2@gmail.com>
2026-05-25 16:14:54 +0200

    6_commit

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-struct Player {
+struct player {
     string playerClass;
 
     int maxHP;
@@ -19,11 +19,22 @@ struct Player {
 
     int attack;
 };
+struct Enemy {
+    string name;
+
+    int hp;
+    int maxHP;
+
+    int attack;
+
+    int xpReward;
+    int goldReward;
+ };
 
 int main() {
     player p;
 
-cout << "Vyber si sveho hrdinu:\n";
+cout << "Vitej ve svete dobrodruzstvi zacni tim ze si vyber sveho statecneho hrdinu:\n";
     cout << "1 - Crusader\n";
     cout << "2 - Mage\n";
      cout << "3 - Hunter\n";
@@ -94,6 +105,66 @@ else if(volba == 2) {
         p.level = 1;
         p.xp = 0;
     }
+Enemy skret;
 
+skret.name = "Skret";
+skret.maxHP = 10;
+skret.hp = 10;
+skret.attack = 2;
+skret.xpReward = 5;
+skret.goldReward = 3;
+
+
+ cout << "narazil jsi na oskliveho skreta!" << endl;
+
+
+while (p.hp > 0 && skret.hp > 0) {
+
+    int action;
+
+    cout << "1 - Attack" << endl;
+    cout << "2 - Heal" << endl;
+
+    cin >> action;
+
+    if (action == 1) {
+
+        skret.hp = skret.hp - p.attack;
+
+        cout << "Uderil jsi skreta!" << endl;
+        cout << "Skret ma " << skret.hp << " HP." << endl;
+    }
+
+    if (skret.hp <= 0) {
+
+        cout << "Skret byl porazen!" << endl;
+    p.xp = p.xp + skret.xpReward;
+    p.gold = p.gold + skret.goldReward;
+
+    cout << "Ziskal jsi XP a gold!" << endl;
+
+        break;
+    }
+
+    if (action == 2) {
+
+        p.hp = p.hp + 5;
+
+        cout << "Vylecil ses!" << endl;
+        cout << "Mas " << p.hp << " HP." << endl;
+    }
+
+    p.hp = p.hp - skret.attack;
+
+    cout << "Skret te uderil!" << endl;
+    cout << "Mas " << p.hp << " HP." << endl;
+
+    if (p.hp <= 0) {
+
+        cout << "Prohral jsi!" << endl;
+
+        break;
+    }
+}
     return 0;
 }
